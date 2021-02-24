75f8942a9ebd9f16e150ced2eb12d10edb72415e
RyanRFelder <ryanrfelder@gmail.com>
2021-02-23 20:23:49 -0800

    fastquant update

--- fastquant_test.py
+++ fastquant_test.py
@@ -1,4 +1,11 @@
-!pip install fastquant
-from fastquant import backtest
+""" Test run space for fastquant
+-For later use on the user's templated or unique portfolio
+"""
+
+##!pip install fastquant
+from fastquant import backtest, get_stock_data
+
+user_portfolio = get_stock_data("AGG","2019-01-01", "2020-12-31")
+
 df = user_portfolio
 backtest("smac",df,fast_period=15,slow_period=40)
